964a20398f4ab27e003005149a43a0e72548bfe7
Maximilian Welsch <maxiwelsch@posteo.de>
2021-12-24 14:12:49 +0100

    start chromium

--- __main__.py
+++ __main__.py
@@ -1,8 +1,11 @@
 # Display must be initalized before fan. The GPIO Initialisation takes splace in the foreign display library
 
 from yt_client.loop import Loop
-
+from yt_client.chromium_control import ChromiumController
 
 if __name__ == '__main__':
+    cc = ChromiumController()
+    cc.start()
+
     l = Loop()
     l.start()
---
+++ yt_client/chromium_control.py
@@ -0,0 +1,16 @@
+import subprocess
+
+# chromium https://invidious.snopyta.org/feed/popular --start-fullscreen
+
+
+class ChromiumController:
+    def __init__(self):
+        self._process = None
+
+    @property
+    def process(self):
+        return self._process
+
+    def start(self):
+        cmd = "chromium https://invidious.snopyta.org/feed/popular --start-fullscreen".split()
+        self._process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
